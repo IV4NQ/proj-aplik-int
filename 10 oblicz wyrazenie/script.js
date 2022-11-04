853fe442b8b73e16affa226aba4b99ea3102b198
2ir02_k02 <iwanowsky1@wp.pl>
2022-11-04 10:26:49 +0100

    commit

---
+++ 10 oblicz wyrazenie/script.js
@@ -0,0 +1,12 @@
+const liczba = document.querySelector('#liczba');
+const wynik = document.querySelector('#wynik');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    let x = parseInt(liczba);
+    let y = Math.abs(x);
+    let dol = 1 + y;
+
+    let wyn = x
+    wynik.innerHTML = 44
+})
