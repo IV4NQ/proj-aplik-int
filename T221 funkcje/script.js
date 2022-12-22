c42befe3d64c51fe8b20fac916275a54dd3054ab
2ir02_k04 <iwanowsky1@wp.pl>
2022-12-22 09:11:42 +0100

    commit

--- T221 funkcje/script.js
+++ T221 funkcje/script.js
@@ -1,28 +1,36 @@
+const liczba_a = document.querySelector('#liczba_a');
+const btn  = document.querySelector('button');
+const wynik = document.querySelector('#wynik');
+
+btn.addEventListener('click', function() {
+    a = parseInt(liczba_a.value)
 // T221
 // ----- Wartosc bezwzgledna sposob 1 -----
-function wartosc_bezwzgledna_1(x){
-    if(x>0){
-        return x;
+    function wartosc_bezwzgledna_1(x) {
+        if (x > 0) {
+            return x;
+        } else {
+            return x * (-1);
+        }
     }
-    else {
-        return x*(-1);
-    }
-}
+
 // ----- Wartosc bezwzgledna sposob 2 -----
-function wartosc_bezwzgledna_2(x){
-    let wynik = (x>0)?x:-x;
-    return wynik;
-}
+    function wartosc_bezwzgledna_2(x) {
+        let wynik = (x > 0) ? x : -x;
+        return wynik;
+    }
+
 // ------------
 
-function zwieksz(x){
-    x = x+1;
-    document.write(x + "<br>");
-}
+    function zwieksz(x) {
+        x = x + 1;
+        document.write(x + "<br>");
+    }
 
-var arg = 15;
-
-zwieksz(arg);
-
-document.write(wartosc_bezwzgledna_1(arg) + "<br>")
-document.write(wartosc_bezwzgledna_2(arg))
+    document.write('wartosc bezwzgledna sposob 1: <br>')
+    document.write(wartosc_bezwzgledna_1(a) + "<br>")
+    document.write('wartosc bezwzgledna sposob 2: <br>')
+    document.write(wartosc_bezwzgledna_2(a) + "<br>")
+    document.write('zwieksz: <br>')
+    document.write(zwieksz(a))
+})
--- T222 zadanie/script.js
+++ T222 zadanie/script.js
@@ -1,11 +1,20 @@
-function potega(x,y){
-    let wynik =1;
-    for(let i=1; i<=x; i++){
-        wynik = x**y;
+const liczba_a = document.querySelector('#liczba_a');
+const liczba_b = document.querySelector('#liczba_b');
+const btn  = document.querySelector('button');
+const wynik = document.querySelector('#wynik');
+
+btn.addEventListener('click', function() {
+    a = parseInt(liczba_a.value)
+    b = parseInt(liczba_b.value)
+
+    function potega(a, b) {
+        let wynik = 1;
+        for (let i = 1; i <= a; i++) {
+            wynik = a ** b;
+        }
+        return wynik;
     }
-    return wynik;
-}
-let x=2;
-let y=3;
-let out = potega(x,y)
-document.write(`${x}<sup>${y}</sup> = ${out}`)
+
+    let out = potega(a, b)
+    wynik.innerHTML = `${a} <sup> ${b} </sup> = ${out}`
+})
