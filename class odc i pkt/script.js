ca68174c5b5f9a68b63a252ce8785a542bd0422f
2ir02 <iwanowsky1@wp.pl>
2023-01-30 08:58:07 +0100

    commit

--- class odc i pkt/script.js
+++ class odc i pkt/script.js
@@ -1,18 +1,34 @@
-class Punkt{
-    nazwa = "PKT SRODKA"
-    x = 12
-    y = 4
-
-    info = function(){
-        console.log(`Nazwa punktu to: ${this.nazwa}. <br> Jego współrzędne to: x = ${this.x} oraz y = ${this.y}`)
+class Punkt {
+    constructor(id, x, y)
+    {
+        this.id = id;
+        this.x = x;
+        this.y = y;
     }
 
-    odcinek = function(){
-
+    info()
+    {
+        document.write(`Punkt "${this.id}" ma współrzędne x: ${this.x}, oraz y: ${this.y}. <br>`);
+    }
 }
 
+class Odcinek {
+    constructor(pkt1, pkt2) {
+        this.pkt1 = pkt1;
+        this.pkt2 = pkt2;
+    }
+    dlugosc()
+    {
+        return Math.sqrt((this.pkt2.x - this.pkt1.x) ** 2 + (this.pkt2.y - this.pkt1.y) ** 2);
+    }
+    info() {
+        document.write(`Odcinek pomiędzy punktami "${this.pkt1.id}" i "${this.pkt2.id}" ma długość ${this.dlugosc().toFixed(2)}.<br>`);
+    }
 }
 
-let punkt = new Punkt;
-punkt.info()
-
+const pktA = new Punkt("A", 2, 3);
+const PktB = new Punkt("B", 4, 5);
+const odcinekAB = new Odcinek(pktA, PktB);
+pktA.info();
+PktB.info();
+odcinekAB.info();
