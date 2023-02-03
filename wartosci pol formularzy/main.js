16ac6be016c0191747ae8daa384430f93ce64e4a
2ir02 <iwanowsky1@wp.pl>
2023-02-03 10:28:22 +0100

    commit

---
+++ wartosci pol formularzy/main.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("#myForm");
+
+console.log(form.firstName.value);
+
+form.lastName.value = "Kowalski";
+
+form.message.value = "Treść";
+
+console.log(form.subject.value);
+
+//form.subject.value = "zgloszenie";
+
+console.log(form.remember.checked);
+
+form.remember.checked = true;
+
+
+const wynik = document.querySelector('#wynik');
+const btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+
+
+    wynik.innerHTML =
+        `Imie: ${form.firstName.value} \
+     <br>Nazwisko: ${form.lastName.value} \
+     <br>Rodzaj: ${form.subject.value} \
+     <br>Treść: ${form.message.value} \
+     <br>zapamiętać cię? ${form.remember.checked}`
+})
