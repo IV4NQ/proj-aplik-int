a8df0e46f1a6a4d7ca33653cbb3eae0f7004c5cc
2ir02 <iwanowsky1@wp.pl>
2023-02-06 10:25:44 +0100

    commit

--- zadania po 30/Zadanie T30a - kolorowe obramowanie obrazu/script.js
+++ zadania po 30/Zadanie T30a - kolorowe obramowanie obrazu/script.js
@@ -1,7 +1,12 @@
 const image = document.getElementById("framed-image");
 const colorSelect = document.getElementById("color-select");
+const colorPicker = document.getElementById("color-picker");
 
 colorSelect.addEventListener("input", function() {
     const hue = this.value;
     image.style.borderColor = `hsl(${hue}, 100%, 50%)`;
+});
+
+colorPicker.addEventListener("input", function() {
+    image.style.borderColor = this.value;
 });
