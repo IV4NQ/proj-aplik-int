570f203c1b94d9ae7c8db73632a759a17533f7f4
2ir02 <iwanowsky1@wp.pl>
2023-04-18 13:25:10 +0200

    commit

---
+++ zadania po 30/T32C/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(buton)
+
+
+btn.addEventListener('click' , function (){
+    const imie = document.querySelector('#imie')
+    const Email = document.querySelector('#Email')
+    const Telefon = document.querySelector('#Telefon')
+
+})
