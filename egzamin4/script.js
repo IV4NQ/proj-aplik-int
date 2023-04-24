0414e4bb38aa0bf56940622c2d04b90d0d5388b7
2ir02 <iwanowsky1@wp.pl>
2023-04-24 10:25:58 +0200

    committ

---
+++ egzamin4/script.js
@@ -0,0 +1,12 @@
+const wyslij = document.querySelector('#wyslij')
+
+
+
+
+
+wyslij.addEventListener('click', function () {
+const imie = document.querySelector('#imie').value
+const nazwisko = document.querySelector('#nazwisko').value
+const Email = document.querySelector('#nazwisko').value
+const usluga = document.querySelector('#usluga')
+})
