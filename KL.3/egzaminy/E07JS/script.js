282921d0afb5aeaa967b0a7696a522a2008ab1f7
Ivan <iwanowsky1@wp.pl>
2023-09-15 12:22:18 +0200

    committ

--- KL.3/egzaminy/E07JS/script.js
+++ KL.3/egzaminy/E07JS/script.js
@@ -1,51 +1,34 @@
-BtnAktDrut = document.querySelector('#BtnAktDrut')
-BtnAktLinka = document.querySelector('#BtnAktLinka')
-BtnAktWtyki = document.querySelector('#BtnAktWtyki')
-BtnAktModuly = document.querySelector('#BtnAktModuly')
+const iloscProduktow = document.querySelectorAll('.ilosc')
+const nazwaProduktow = document.querySelectorAll('.produkt')
+const aktualizujPrzycisk = document.querySelectorAll('.aktualizuj')
+const zamowPrzycisk = document.querySelectorAll('.zamow')
+let idZamowienia = 0
 
-BtnZamDrut = document.querySelector('#BtnZamDrut')
-BtnZamLinka = document.querySelector('#BtnZamLinka')
-BtnZamWtyki = document.querySelector('#BtnZamWtyki')
-BtnZamModuly = document.querySelector('#BtnZamModuly')
-
-
-
-function sprawdz(){
-    iloscDrut = document.getElementById(iloscDrut).value
-    iloscLinka = document.getElementById(iloscLinka).value
-    iloscWtyki = document.getElementById(iloscWtyki).value
-    iloscModuly = document.getElementById(iloscModuly).value
-
-    if (iloscDrut == 0){
-        iloscDrut.style.backgroundColor = "red"
-    } else if(iloscDrut > 0 && iloscDrut <= 5){
-        iloscDrut.style.backgroundColor = "yellow"
-    } else {
-        iloscDrut.style.backgroundColor = "Honeydew"
-    }
-
-    if (iloscLinka == 0){
-        iloscLinka.style.backgroundColor = "red"
-    } else if(iloscLinka > 0 && iloscLinka <= 5){
-        iloscLinka.style.backgroundColor = "yellow"
-    } else {
-        iloscLinka.style.backgroundColor = "Honeydew"
-    }
-
-    if (iloscWtyki == 0){
-        iloscWtyki.style.backgroundColor = "red"
-    } else if(iloscWtyki > 0 && iloscWtyki <= 5){
-        iloscWtyki.style.backgroundColor = "yellow"
-    } else {
-        iloscWtyki.style.backgroundColor = "Honeydew"
-    }
-
-    if (iloscModuly == 0){
-        iloscModuly.style.backgroundColor = "red"
-    } else if(iloscModuly > 0 && iloscModuly <= 5){
-        iloscModuly.style.backgroundColor = "yellow"
-    } else {
-        iloscModuly.style.backgroundColor = "Honeydew"
-    }
+function sprawdz() {
+    iloscProduktow.forEach((produkt) => {
+        if (produkt.innerHTML == 0) {
+            produkt.style = 'background-color: red;'
+        }
+        else if (produkt.innerHTML >= 1 && produkt.innerHTML <= 5) {
+            produkt.style = 'background-color: yellow;'
+        }
+        else{
+            produkt.style = 'background-color: Honeydew;'
+        }
+    })
 }
+
+aktualizujPrzycisk.forEach((przycisk, i) => {
+    przycisk.addEventListener('click', () => {
+        iloscProduktow[i].innerHTML = prompt(`Podaj nową ilość:`)
+        sprawdz()
+    })
+})
+
+zamowPrzycisk.forEach((przycisk, i) => {
+    przycisk.addEventListener('click', () => {
+        alert(`Zamówienie nr: ${idZamowienia} Produkt: ${nazwaProduktow[i].innerHTML}`)
+        idZamowienia++
+    })
+})
 sprawdz()
